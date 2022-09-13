b1dddf2c8d2de06a47a1eb044be91489526a707d
katerinasolomakha <katenokgav7@gmail.com>
2022-09-13 23:26:57 +0300

    Add task8.py and task9.py

---
+++ task9.py
@@ -0,0 +1,13 @@
+from re import S
+
+n = int(input())
+line = 1
+i = 1
+while i <= n:
+    s = " "
+    for _ in range(line):
+        s = str(i) + " " + s
+        i += 1
+        if i > n: break
+    print(s)
+    line += 1
